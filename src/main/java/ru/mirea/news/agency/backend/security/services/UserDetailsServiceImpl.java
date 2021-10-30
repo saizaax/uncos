40a8556db4897d6f4145721e93d234902ed217d0
saizaax <flamesuspect@gmail.com>
2021-10-30 18:53:42 +0300

    Added UserDetailsService implementation class.

---
+++ src/main/java/ru/mirea/news/agency/backend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.mirea.news.agency.backend.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.mirea.news.agency.backend.entity.User;
+import ru.mirea.news.agency.backend.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
