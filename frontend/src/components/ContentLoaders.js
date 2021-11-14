00721975417e119f05bee0e5a16f17f9495f28bb
saizaax <flamesuspect@gmail.com>
2021-11-14 12:48:22 +0300

    Added ContentLoaders component.

---
+++ frontend/src/components/ContentLoaders.js
@@ -0,0 +1,37 @@
+import ContentLoader from "react-content-loader"
+
+export const MainContentLoader = () => {
+  return (
+    <ContentLoader width={"100%"} height={"670"}>
+      <rect x="0" y="0" rx="4" ry="4" width="100%" height="300" />
+      <rect x="0" y="310" rx="4" ry="4" width="32%" height="45" />
+      <rect x="0" y="365" rx="4" ry="4" width="32%" height="25" />
+      <rect x="0" y="400" rx="4" ry="4" width="32%" height="250" />
+      <rect x="33%" y="310" rx="4" ry="4" width="34%" height="45" />
+      <rect x="33%" y="365" rx="4" ry="4" width="34%" height="25" />
+      <rect x="33%" y="400" rx="4" ry="4" width="34%" height="250" />
+      <rect x="68%" y="310" rx="4" ry="4" width="32%" height="45" />
+      <rect x="68%" y="365" rx="4" ry="4" width="32%" height="25" />
+      <rect x="68%" y="400" rx="4" ry="4" width="32%" height="250" />
+    </ContentLoader>
+  )
+}
+
+export const ArticleContentLoader = () => {
+  return (
+    <ContentLoader width={"100%"} height={"690"}>
+      <rect x="0" y="0" rx="4" ry="4" width="70%" height="340" />
+      <rect x="71%" y="0" rx="4" ry="4" width="29%" height="45" />
+      <rect x="71%" y="55" rx="4" ry="4" width="29%" height="25" />
+      <rect x="71%" y="65" rx="4" ry="4" width="29%" height="275" />
+      <rect x="0" y="350" rx="4" ry="4" width="70%" height="30" />
+      <rect x="0" y="390" rx="4" ry="4" width="70%" height="25" />
+      <rect x="0" y="425" rx="4" ry="4" width="70%" height="25" />
+      <rect x="0" y="460" rx="4" ry="4" width="70%" height="55" />
+      <rect x="0" y="525" rx="4" ry="4" width="70%" height="190" />
+      <rect x="71%" y="350" rx="4" ry="4" width="29%" height="45" />
+      <rect x="71%" y="405" rx="4" ry="4" width="29%" height="25" />
+      <rect x="71%" y="440" rx="4" ry="4" width="29%" height="275" />
+    </ContentLoader>
+  )
+}
